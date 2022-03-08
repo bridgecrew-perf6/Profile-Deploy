249a44aa2281e413e3e98104959c4461181171a0
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2022-03-07 20:58:16 -0500

    Finished Contact page only awaiting projects section.

---
+++ profile/src/components/grids/ProjectData.js
@@ -0,0 +1,14 @@
+import { meImg } from '../../Profile-Pics';
+
+export const ProjectData = [
+	{
+		id: 'Profile_Project',
+		pic: meImg,
+		link: 'https://eds-dbug.github.io/Profile-Deploy/',
+		description:
+			'{I need a better profile pic} This was(is) my very first react app. At first it was a normal website made with React but as I delved into I started getting creative with my code eventually I started to make more better and reusable code rather than just HTML and CSS tags.                                                                 ',
+	},
+	{
+		id: '',
+	},
+];
